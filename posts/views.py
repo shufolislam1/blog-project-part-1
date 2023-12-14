117fa8cffc95e0557e5d037af53bf15abc36338d
shufolislam1 <shufolislam1255@gmail.com>
2023-12-14 16:03:39 +0600

    post added

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . import forms
 
 # Create your views here.
+def add_post(request):
+    if request.method == 'POST':
+        post_form = forms.PostForm(request.POST)
+        if post_form.is_valid():
+            post_form.save()
+            return redirect('add_post')
+    else:
+        post_form = forms.PostForm()
+    return render(request,'add_post.html', {'post_form': post_form})
