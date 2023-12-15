961211758b33c6c270f3f30829e51062c45cf865
shufolislam1 <shufolislam1255@gmail.com>
2023-12-15 12:30:38 +0600

    edit done

--- posts/urls.py
+++ posts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add/', views.add_post, name='add_post')
+    path('add/', views.add_post, name='add_post'),
+    path('edit/<int:id>', views.edit_post, name='edit_post') 
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from . import forms
+from . import models
 
 # Create your views here.
 def add_post(request):
@@ -7,7 +8,18 @@ def add_post(request):
         post_form = forms.PostForm(request.POST)
         if post_form.is_valid():
             post_form.save()
-            return redirect('add_post')
+            return redirect('home')
     else:
         post_form = forms.PostForm()
+    return render(request,'add_post.html', {'post_form': post_form})
+
+
+def edit_post(request, id):
+    post = models.Post.objects.get(pk=id)
+    post_form = forms.PostForm(instance=post)
+    if request.method == 'POST':
+        post_form = forms.PostForm(request.POST, instance=post)
+        if post_form.is_valid():
+            post_form.save()
+            return redirect('home')
     return render(request,'add_post.html', {'post_form': post_form})
